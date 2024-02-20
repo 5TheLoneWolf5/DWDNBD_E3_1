fc7e87b0550007b3bd36e03b375cae85eb456819
5TheLoneWolf5 <89516641+5TheLoneWolf5@users.noreply.github.com>
2024-02-19 22:01:27 -0300

    New commit.

---
+++ Pages/CadastroProduto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DWDNBD_E3_1.Pages
+{
+    public class CadastroProdutoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
